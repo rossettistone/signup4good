aaa50e54069ea109f61fc6835da0da5a85fe47ff
Jesse Aldridge <jessealdridge@gmail.com>
2014-01-25 11:40:45 -0800

    it's compiling

--- server/server.js
+++ server/server.js
@@ -8,21 +8,14 @@ Accounts.onCreateUser(function(options, user) {
 
 function create_event(org_id) {
   return {
+    event_name:'best event ever',
     num_slots:10, type:'management', is_one_time:true, org_id:[org_id]
   }
 }
 
-function create_organization() {
-  return {
-    org_name:'Foo Org', email:'foo@bar.org'
-    contact_name:'Bob Organizer', address:'555 Foo Lane',
-    event_ids:[]
-  }
-}
-
 Meteor.startup(function () {
-  Orgs = new Meteor.Collection("orgs");
-  Events = new Meteor.Collection("events");
+  Org = new Meteor.Collection("orgs");
+  Event = new Meteor.Collection("events");
 
   // Organizations can create accounts
   //   Email Address
@@ -36,38 +29,40 @@ Meteor.startup(function () {
   //   -Location
 
   // code to run on server at startup
-  if (Organization.find().count() === 0) {
+  if (Org.find().count() === 0) {
     default_org = {
       'point_of_contact':'Max Org',
       address:'555 Foo Lane', email:'foo@bar.org', phone:'555-555-5555',
-      waitlist:[], event_ids:[]}
+      waitlist:[], event_ids:[]
+    }
 
     var org_names = ['Foo Org', 'Bar Org', 'Baz Org'];
     for (var i = 0; i < org_names.length; i++) {
       default_org['name'] = org_names[i]
-      Organization.insert(default_org, function (err, org_id) {
+      Org.insert(default_org, function (err, org_id) {
         var event_names = ['Foo Event', 'Bar Event', 'Baz Event']
         for(var i = 0; i < event_names; i++) {
           new_event = create_event(org_id)
           new_event.name = event_names[i]
-          new_org = Organization.findOne({_id:org_id})
-          new_org.event_ids.push(new_event._id)
           Event.insert(new_event)
+          Org.update(
+            {_id:org_id}, 
+            {$push:{event_ids:new_event._id}})
         }
-      }
+      })
     }
   }
 
-  if (Volunteer.find().count() === 0) {
-    default_volunteer = {
-      'name':'Bob Volun',
-      address:'777 Bar Road', email:'bar@volun.com', phone:'555-555-5555',
-      waitlist:[]}
+  // if (Volunteer.find().count() === 0) {
+  //   default_volunteer = {
+  //     'name':'Bob Volun',
+  //     address:'777 Bar Road', email:'bar@volun.com', phone:'555-555-5555',
+  //     waitlist:[]}
 
-    var names = ["Foo Org", "Bar Org", "Baz Org"];
-    for (var i = 0; i < names.length; i++) {
-      default_org['name'] = names[i]
-      Volunteer.insert(default_org)
-    }
-  }
+  //   var names = ["Foo Org", "Bar Org", "Baz Org"];
+  //   for (var i = 0; i < names.length; i++) {
+  //     default_org['name'] = names[i]
+  //     Volunteer.insert(default_org)
+  //   }
+  // }
 });
