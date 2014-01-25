de86cd5ddb6ab14216db740b91ef84f12fe9ba62
Jesse Aldridge <jessealdridge@gmail.com>
2014-01-25 11:33:22 -0800

    more work on stuffing db

--- server/server.js
+++ server/server.js
@@ -6,10 +6,17 @@ Accounts.onCreateUser(function(options, user) {
     return user;
 });
 
-function default_event(org_id) {
+function create_event(org_id) {
   return {
-    name:'Best Event Ever', num_slots:10, type:'management',
-    is_one_time:false, address:'88 Baz Boulevard', org_id:org_id
+    num_slots:10, type:'management', is_one_time:true, org_id:[org_id]
+  }
+}
+
+function create_organization() {
+  return {
+    org_name:'Foo Org', email:'foo@bar.org'
+    contact_name:'Bob Organizer', address:'555 Foo Lane',
+    event_ids:[]
   }
 }
 
@@ -18,9 +25,6 @@ Meteor.startup(function () {
   Events = new Meteor.Collection("events");
 
   // Organizations can create accounts
-  //   Org name
-  //   Point of Contact Name
-  //   Physical Address
   //   Email Address
   //   Phone
   //   Waitlist (cap?)
@@ -31,39 +35,39 @@ Meteor.startup(function () {
   //   -One-time or repeating
   //   -Location
 
-
   // code to run on server at startup
   if (Organization.find().count() === 0) {
+    default_org = {
+      'point_of_contact':'Max Org',
+      address:'555 Foo Lane', email:'foo@bar.org', phone:'555-555-5555',
+      waitlist:[], event_ids:[]}
 
-    function default_org() {
-      return  {
-        'point_of_contact':'Max Org',
-        address:'555 Foo Lane', email:'foo@bar.org', phone:'555-555-5555',
-        waitlist:[], event_ids:[]}
+    var org_names = ['Foo Org', 'Bar Org', 'Baz Org'];
+    for (var i = 0; i < org_names.length; i++) {
+      default_org['name'] = org_names[i]
+      Organization.insert(default_org, function (err, org_id) {
+        var event_names = ['Foo Event', 'Bar Event', 'Baz Event']
+        for(var i = 0; i < event_names; i++) {
+          new_event = create_event(org_id)
+          new_event.name = event_names[i]
+          new_org = Organization.findOne({_id:org_id})
+          new_org.event_ids.push(new_event._id)
+          Event.insert(new_event)
+        }
       }
     }
+  }
 
-    default_event = {
-      name:'Best Event Ever', num_slots:10, type:'management',
-      is_one_time:false, address:'88 Baz Boulevard', org_id:org_id}
-
-    var default_events = []
-    var names = ['Default Event 1', 'Default Event 2']
-
-
-    var names = ["Foo Org", "Bar Org", "Baz Org"]
-    for (var i = 0; i < names.length; i++) {
-      default_org['name'] = names[i]
-
-      Event.insert(default_org)
-    }
+  if (Volunteer.find().count() === 0) {
+    default_volunteer = {
+      'name':'Bob Volun',
+      address:'777 Bar Road', email:'bar@volun.com', phone:'555-555-5555',
+      waitlist:[]}
 
     var names = ["Foo Org", "Bar Org", "Baz Org"];
     for (var i = 0; i < names.length; i++) {
       default_org['name'] = names[i]
-      Organization.insert(default_org)
+      Volunteer.insert(default_org)
     }
   }
-
-
 });
